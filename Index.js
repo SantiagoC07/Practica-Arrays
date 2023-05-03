cd3e1227140d967ff1b0d48e514b050724e4a324
Santiago Andres Comas Duran <Santiagocomas079@gmail.com>
2023-05-02 23:39:03 -0500

    Practica Arrays 04/05/2023

---
+++ Index.js
@@ -0,0 +1,28 @@
+
+
+
+
+const Array = ["Santiago", "Sara", "Sofia", "Daniel" ];
+
+// let watch = Array[0];
+
+// console.log (watch);
+
+for (let i =0; i < 4; i++ ){
+
+
+let contador = Array[i];
+
+console.log(contador);
+
+
+}
+
+
+
+
+
+
+
+
+
